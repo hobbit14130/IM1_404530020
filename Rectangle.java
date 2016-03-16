ab2062643b65d670c99bf91c2de1fcfe3e153af8
hobbit14130 <hobbit14130@gmail.com>
2016-03-17 00:27:29 +0800

    Added files via upload

---
+++ Rectangle.java
@@ -0,0 +1,55 @@
+ class RectangleTester {
+		 String name;
+		 double Height;
+		 double Width;
+		 double x ;
+		 double y ;
+		public RectangleTester(String name,double Heigh,double Width,double x,double y) {
+			this.name=name;
+			this.Height=Heigh;
+			this.Width=Width;
+			this.x=x;
+			this.y=y;	
+					
+		}
+
+
+		
+		double getHeigh(){
+			return this.Height;
+		}
+		double getWidth(){
+			return this.Width;
+		}
+		
+
+		public String toString(){
+			return "java.Rectangle[x="+this.x+",y="+this.y+",width="+this.Width+",heigh="+this.Height+"]";
+		}
+		//上面是為了覆寫格式 讓他符合我們要的格式
+
+		}
+ 
+	
+
+public class Rectangle {
+	
+	public static void main(String[] args) {
+		RectangleTester S1= new RectangleTester("S1",18,57,15,25);
+		
+		System.out.println(S1);
+		System.out.println("Area="+S1.getHeigh()*S1.getWidth());
+		System.out.println("Perimeter="+(S1.getHeigh()+S1.getWidth())*2);
+		RectangleTester S2= new RectangleTester("S1",47,60,0,12);
+		System.out.println(S2);
+		System.out.println("Area="+S2.getHeigh()*S2.getWidth());
+		System.out.println("Perimeter="+(S2.getHeigh()+S2.getWidth())*2);
+		System.out.println("程式結束");
+	}
+	
+	
+	
+	
+	
+}
+
